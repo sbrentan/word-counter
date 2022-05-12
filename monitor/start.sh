c1b4814a6a9a94352b0953369965a23964e84680
Simone <simobrentan@gmail.com>
2022-05-12 20:14:51 +0200

    Revert "Added bash start commands"
    This reverts commit 6ae5412cbf009e32848d8d1ace6b247e6202f1c6.

--- monitor/start.sh
+++
@@ -1,6 +0,0 @@
-docker build -t monitor monitor/
-p1=10
-p2=$(( $1 + 10 ))
-p3=$(( $1 + 20 ))
-p4=$(( $1 + 30 ))
-docker run -d -p 8080:80 -p $p1:$p1 -p $p2:$p2 -p $p3:$p3 -p $p4:$p4 -e EXP_PORT=$p1 -e ACCEPT_MODE=passive --name=monitor --network=word-counter-net -v $PWD/monitor:/monitor -v $PWD/master:/master monitor
--- node/client-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t $1 node/
-docker run -d --network=word-counter-net --name=$1 -e MASTER_IP=master -e MASTER_PORT=$2 -e GENERATE=doc1.txt -v $PWD/node:/node $1
